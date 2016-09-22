5f305aef5eb36254c5eb9063cf56993b2b9932f6
RWejlgaard <wejlgaard@protonmail.com>
2016-09-22 10:50:15 +0200

    Implemented Material Theme

--- SmartArchive-Util/Program.cs
+++ SmartArchive-Util/Program.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Pyratron.Frameworks.Commands.Parser;
 
-namespace SmartArchive_Util
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
+namespace SmartArchive_Util {
+    class Program {
+
+        static void Main(string[] args) {
+            var parser = CommandParser.CreateNew("--");
+            parser.AddCommand(Command.Create("Hello").AddAlias("hello").SetDescription("Prints: 'Hello World!'")
+                .SetAction((arguments, data) => {
+                    Console.WriteLine("Hello World!");
+                }));
+            if (args.Length != 0) {
+                parser.Parse(args[0]);
+            }
         }
     }
 }
